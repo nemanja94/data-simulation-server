4097bc139fe2b23d13b731ee89a25c815c4d318e
ionevencae <veljko.aleksijevic@gmail.com>
2024-08-03 21:27:50 +0200

    blabla nista spec

--- express-json-sender/server.js
+++ express-json-sender/server.js
@@ -8,82 +8,88 @@ const port = 3000;
 app.use(express.json());
 
 // Start sending data every 5 seconds
-const targetUrl = 'api'; // TODO
+const targetUrl = 'http://localhost:3000/data'; // TODO
+
+let dataJson = {
+    "greenhouse_id": "1",
+    "greenhouse_name": "Greenhouse 1",
+    "date": "2021-06-01T00:00:00.000Z",
+    "bar_quantity": 3,
+    "bars_data": {
+        "1": {
+            "bar_id": "1",
+            "bar_name": "Bar 1",
+            "data": {
+                "humidity": {
+                    "zone_n10": 0.5,
+                    "zone_30": 0.8,
+                    "zone_120": 0.9
+                },
+                "temperature": {
+                    "zone_n10": 0.5,
+                    "zone_30": 0.8,
+                    "zone_120": 0.9
+                },
+                "ph": 1,
+                "air_pressure": 1000
+            }
+        },
+        "2": {
+            "bar_id": "1",
+            "bar_name": "Bar 1",
+            "data": {
+                "humidity": {
+                    "zone_n10": 0.5,
+                    "zone_30": 0.8,
+                    "zone_120": 0.9
+                },
+                "temperature": {
+                    "zone_n10": 0.5,
+                    "zone_30": 0.8,
+                    "zone_120": 0.9
+                },
+                "ph": 1,
+                "air_pressure": 1000
+            }
+        },
+        "3": {
+            "bar_id": "1",
+            "bar_name": "Bar 1",
+            "data": {
+                "humidity": {
+                    "zone_n10": 0.5,
+                    "zone_30": 0.8,
+                    "zone_120": 0.9
+                },
+                "temperature": {
+                    "zone_n10": 0.5,
+                    "zone_30": 0.8,
+                    "zone_120": 0.9
+                },
+                "ph": 1,
+                "air_pressure": 1000
+            }
+        }
+    }
+}
 
 function sendJsonData() {
     const jsonData = {
         timestamp: new Date().toISOString(),
-        data: `{
-            "greenhouse_id": "1",
-            "greenhouse_name": "Greenhouse 1",
-            "date": "2021-06-01T00:00:00.000Z",
-            "bar_quantity": 3,
-            "bars_data": {
-                "1": {
-                    "bar_id": "1",
-                    "bar_name": "Bar 1",
-                    "data": {
-                        "humidity": {
-                            "zone_n10": 0.5,
-                            "zone_30": 0.8,
-                            "zone_120": 0.9
-                        },
-                        "temperature": {
-                            "zone_n10": 0.5,
-                            "zone_30": 0.8,
-                            "zone_120": 0.9
-                        },
-                        "ph": 1,
-                        "air_pressure": 1000
-                    }
-                },
-                "2": {
-                    "bar_id": "1",
-                    "bar_name": "Bar 1",
-                    "data": {
-                        "humidity": {
-                            "zone_n10": 0.5,
-                            "zone_30": 0.8,
-                            "zone_120": 0.9
-                        },
-                        "temperature": {
-                            "zone_n10": 0.5,
-                            "zone_30": 0.8,
-                            "zone_120": 0.9
-                        },
-                        "ph": 1,
-                        "air_pressure": 1000
-                    }
-                },
-                "3": {
-                    "bar_id": "1",
-                    "bar_name": "Bar 1",
-                    "data": {
-                        "humidity": {
-                            "zone_n10": 0.5,
-                            "zone_30": 0.8,
-                            "zone_120": 0.9
-                        },
-                        "temperature": {
-                            "zone_n10": 0.5,
-                            "zone_30": 0.8,
-                            "zone_120": 0.9
-                        },
-                        "ph": 1,
-                        "air_pressure": 1000
-                    }
-                }
-            }
-        }`
+        data: dataJson
     };
 
-    axios.post(targetUrl, jsonData)
-        .then(response => {
-            console.log(`Data sent successfully: ${response.status}`);
-        })
-        .catch(error => {
-            console.error(`Error sending data: ${error.message}`);
-        });
+    app.get('/data', (req, res) => {
+        res.json(jsonData);
+    });
+
+    // axios.post(targetUrl, jsonData)
+    //     .then(response => {
+    //         console.log(`Data sent successfully: ${response.status}`);
+    //     })
+    //     .catch(error => {
+    //         console.error(`Error sending data: ${error.message}`);
+    //     });
 }
 
 // Send JSON data every 5 seconds
